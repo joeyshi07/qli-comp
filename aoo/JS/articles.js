5c557fef9582960a93a2034e8467ff6ad7059b3e
joeyshi07 <joeys19@nycstudents.net>
2025-04-20 23:55:03 -0400

    changing content

--- aoo/JS/articles.js
+++ aoo/JS/articles.js
@@ -1,15 +1,15 @@
 export const articles = [
   {
     title: "ESM-2 And Variant Classification",
-    author: "Joey Shi",
+    author: "Student Author",
     description:
-      "This blog details how ESM-2 can be utilized in predicting whether a mutation is disease causing or neutral",
+      "We all know of ChatGPT, a language learning model trained to string connections between information. This can now be applied to protein sequences as well---and medical implications along with it",
     imageLink:
       "https://media.npr.org/assets/img/2013/01/29/highres-fleetwood-mac-rumours_sq-496d22b156d14a8c7e5fbfd77ab1d54790d019b3.jpg?s=1100&c=50&f=jpeg",
   },
   {
-    title: "ESM-2 And Variant Classification",
-    author: "Joey Shi",
+    title: "AlphaFold-2 and the Protein Folding Problem",
+    author: "Student Author",
     description:
       "This blog details how ESM-2 can be utilized in predicting whether a mutation is disease causing or neutral",
     imageLink:
